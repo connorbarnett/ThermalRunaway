116370803c0f209089f462fd1170cb2ca54b5784
Robert Dunlevie <robdun11@stanford.edu>
2014-04-14 16:06:57 -0700

    Adding basic Rails app

---
+++ rails/ThermalRails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThermalRails::Application.config.secret_key_base = 'ea169be4fa127e1a89c77b313066fd40b0df7d1edc8d257ef7bfaf57aeea98c6d4d58ff59311b26d95c190e51a1657f5dce205bd4141254dc08e9a465cd241b1'
