d9e35003f53752780a9108e289e78a1e0a621c7c
Robert Dunlevie <robdun11@stanford.edu>
2014-04-14 21:32:51 -0700

    added imgurl migration

--- rails/ThermalRails/db/schema.rb
+++ rails/ThermalRails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414231146) do
+ActiveRecord::Schema.define(version: 20140415043233) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140414231146) do
     t.integer  "down_votes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "img_url"
   end
 
 end
