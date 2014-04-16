1e20116f4f2ff81c650ec5de32e9f657403f6e77
Robert Dunlevie <robdun11@stanford.edu>
2014-04-15 20:28:49 -0700

    added Votes model/sql3ite db

--- rails/ThermalRails/db/schema.rb
+++ rails/ThermalRails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415051313) do
+ActiveRecord::Schema.define(version: 20140416004619) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20140415051313) do
     t.datetime "updated_at"
     t.string   "img_url"
     t.integer  "num_unknown"
+  end
+
+  create_table "votes", force: true do |t|
+    t.string   "company"
+    t.string   "vote_type"
+    t.string   "vote_location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
