29720861a0442f622493ec11fc0d3f477e894d51
Robert Dunlevie <robdun11@stanford.edu>
2014-04-19 14:38:37 -0700

    added img_url to json extract

--- HoNVC.m
+++ HoNVC.m
@@ -25,11 +25,11 @@
     [super viewDidLoad];
     [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"nest" andUrl:@"http://www.technewsworld.com/images/rw734591/home-energy-consumption.jpg"]];
     [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"Lyft" andUrl:@"http://upload.wikimedia.org/wikipedia/commons/4/48/Lyft_logo.jpg"]];
-    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"facebook" andUrl:@"http://www.underconsideration.com/brandnew/archives/facebook_logo_detail.gif"]];
-    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"nextdoor" andUrl:@"https://nextdoor.com/static/nextdoorv2/images/newsroom/logo-white-large.png"]];
-    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"asana" andUrl:@"http://readwrite.com/files/files/files/enterprise/images/asana_logo_0411.png"]];
-    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"snapchat" andUrl:@"http://upload.wikimedia.org/wikipedia/en/5/5e/Snapchat_logo.png"]];
-    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"soundcloud" andUrl:@"http://www.bobsima.com/img/Widgets/SoundCloud_Color.png"]];
+//    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"facebook" andUrl:@"http://www.underconsideration.com/brandnew/archives/facebook_logo_detail.gif"]];
+//    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"nextdoor" andUrl:@"https://nextdoor.com/static/nextdoorv2/images/newsroom/logo-white-large.png"]];
+//    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"asana" andUrl:@"http://readwrite.com/files/files/files/enterprise/images/asana_logo_0411.png"]];
+//    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"snapchat" andUrl:@"http://upload.wikimedia.org/wikipedia/en/5/5e/Snapchat_logo.png"]];
+//    [self.view addSubview:[[DraggableView alloc] initWithFrame:CGRectMake(20, 130, 200, 260) company:@"soundcloud" andUrl:@"http://www.bobsima.com/img/Widgets/SoundCloud_Color.png"]];
 
 }
 
--- rails/ThermalRails/app/views/companies/index.json.jbuilder
+++ rails/ThermalRails/app/views/companies/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@companies) do |company|
-  json.extract! company, :id, :name
+  json.extract! company, :id, :name, :img_url
   json.url company_url(company, format: :json)
 end
