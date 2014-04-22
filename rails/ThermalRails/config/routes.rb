5183cda82cc2aca0e2ccfa458dbf90d3b08f3dcf
Robert Dunlevie <robdun11@stanford.edu>
2014-04-22 00:05:44 -0700

    one liner, switched vote to post

--- rails/ThermalRails/config/routes.rb
+++ rails/ThermalRails/config/routes.rb
@@ -1,7 +1,7 @@
 ThermalRails::Application.routes.draw do
   get "test/index"
   resources :companies
-  match '/vote',  to: 'companies#vote',       via: 'get'
+  match '/vote',  to: 'companies#vote',       via: 'post'
   match '/vote.json', to: 'companies#vote', via: 'post'
   match '/company/lookup', to: 'companies#lookup',    via: 'get'
   match '/vote/lookup', to: 'companies#voteLookup', via: 'get'
