aadfd89454a9f8ae1fe765a1a084f709b12bdc93
Robert Dunlevie <robdun11@stanford.edu>
2014-05-11 17:30:28 -0700

    removed csrf warning, need to get company_id in comparison model still

--- rails/ThermalRails/app/controllers/companies_controller.rb
+++ rails/ThermalRails/app/controllers/companies_controller.rb
@@ -1,5 +1,6 @@
 class CompaniesController < ApplicationController
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+  skip_before_filter :verify_authenticity_token, :only => [:compare]
 
   before_action :set_company, only: [:show, :edit, :update, :destroy]
 
