b71ff3be4f671c4030054416fb65e8da815d0291
Robert Dunlevie <robdun11@stanford.edu>
2014-05-28 23:44:15 -0700

    added description migration, need to rake after pull

--- rails/ThermalRails/db/schema.rb
+++ rails/ThermalRails/db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529031030) do
+ActiveRecord::Schema.define(version: 20140529064353) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
   create_table "comparisons", force: true do |t|
